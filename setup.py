dec2ba3a5d01516c0aa745b1d1b3cebfffeb3974
Florian Fabre <ffabre@hybird.org>
2018-03-09 14:26:51 +0100

    Fix requests-mock version requirement (>=1.2.0)

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     install_requires=[
         'attrs',
         'funcsigs',
-        'requests-mock<2.0.0',
+        'requests-mock>=1.2.0',
     ],
     extras_require={
         'test': [
